a7125a071b85addb2e887a391d92e765200be13a
Volodymyr Zholobenko <vzholobenko@pc07.pool.tp1.uni-duesseldorf.de>
2015-11-10 12:18:11 +0100

    done

--- newton.cxx
+++ newton.cxx
@@ -14,12 +14,12 @@ int main(){
   complex<double> z1;
   int i = 0;
   
-  for(double re = -2; re <=2; re+=0.005){
-    for(double im = -2; im<=2; im+=0.005){
+  for(double re = -2; re <=2; re+=0.01){
+    for(double im = -2; im<=2; im+=0.01){
       z0 = complex<double>(re,im);
       z1 = z0 - (z0*z0*z0-1.0)/(3.0*z0*z0);
       i=0;
-      while ( (abs(z1-z0)>1e-8) && (i<200) ){
+      while ( (abs(z1-z0)>1e-8) && (i<30) ){
 	z0 = z1;
 	z1 -= (z1*z1*z1-1.0)/(3.0*z1*z1);
 	i++;
