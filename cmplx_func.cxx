b75c1e57fc17f3770ba422774c978e998f0fe8c6
Volodymyr Zholobenko <vzholobenko@pc07.pool.tp1.uni-duesseldorf.de>
2015-11-10 12:05:07 +0100

    done

--- cmplx_func.cxx
+++ cmplx_func.cxx
@@ -1,13 +1,29 @@
-#include <iostream>
 #include <complex>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+complex<double> tang(double phi) {
+  complex<double> ii = complex<double>(0.0, 1.0);
+  return tan(phi*(1.0+ii));
+}
 
 int main(){
-
-  // Example how to declare a single complex variable:
-  complex<double> c;
-
-  // d = 1 + 2i 
-  complex<double> d = complex<double>(1.0, 2.0); 
-
+  
+  complex<double> ii = complex<double>(0.0, 1.0);
+  int N = 100;
+  double phi[N];
+  phi[0] = -M_PI;
+  double d_phi = 2*M_PI/N;
+  complex<double> f[N];
+  for(int i=0; i<N; i++) {
+    //f[i] = exp(phi[i]*ii);
+    f[i] = tang(phi[i]);
+    phi[i+1] = phi[i] + d_phi;
+  }
+  for(int i=0; i<N; i++) {
+    cout <<phi[i]<< "\t" <<real(f[i])<< "\t" <<imag(f[i])<< "\t" <<abs(f[i])<< endl;
+  }
   return 0;
 }
--- newton.cxx
+++ newton.cxx
@@ -1,13 +1,32 @@
 #include <iostream>
 #include <complex>
 
+using namespace std;
+
 int main(){
 
   // Example how to declare a single complex variable:
-  complex<double> c;
-
+  //complex<double> c;
   // d = 1 + 2i 
-  complex<double> d = complex<double>(1.0, 2.0); 
+  //complex<double> d = complex<double>(1.0, 2.0); 
+  
+  complex<double> z0;
+  complex<double> z1;
+  int i = 0;
+  
+  for(double re = -2; re <=2; re+=0.005){
+    for(double im = -2; im<=2; im+=0.005){
+      z0 = complex<double>(re,im);
+      z1 = z0 - (z0*z0*z0-1.0)/(3.0*z0*z0);
+      i=0;
+      while ( (abs(z1-z0)>1e-8) && (i<200) ){
+	z0 = z1;
+	z1 -= (z1*z1*z1-1.0)/(3.0*z1*z1);
+	i++;
+      }
+      cout << re << "\t" << im << "\t" << i << endl;
+    }
+  }
 
   return 0;
 }
